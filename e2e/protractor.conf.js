08c712e08e5360063d8898a099ccac4478f588bd
Michel Palourdio <mpalourdio@gmail.com>
2020-04-18 13:39:27 +0200

    Pagan Poetry

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -3,6 +3,7 @@
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
+const chromeExecutablePath = require('puppeteer').executablePath();
 
 /**
  * @type { import("protractor").Config }
@@ -12,16 +13,40 @@ exports.config = {
   specs: [
     './src/**/*.e2e-spec.ts'
   ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
+  multiCapabilities: [
+    {
+      browserName: 'chrome',
+      chromeOptions: {
+        binary: chromeExecutablePath,
+        args: [
+          '--headless',
+          '--no-sandbox',
+          '--disable-gpu',
+          // To make sure we have a form factor that's not a phone
+          '--window-size=2000,1000',
+          '--disable-browser-side-navigation',
+          '--disable-extensions',
+          '--disable-dev-shm-usage',
+        ],
+      },
+    },
+    {
+      browserName: 'firefox',
+      'moz:firefoxOptions': {
+        args: [
+          '--headless',
+        ],
+      },
+    },
+  ],
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () {
+    }
   },
   onPrepare() {
     require('ts-node').register({
