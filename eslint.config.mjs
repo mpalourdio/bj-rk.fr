77c8932ab65af8d9209b232033d135babfefce1c
Michel Palourdio <mpalourdio@gmail.com>
2026-06-08 23:17:50 +0000

    Body Memory

---
+++ eslint.config.mjs
@@ -0,0 +1,95 @@
+import eslint from '@eslint/js';
+import {defineConfig} from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import angular from 'angular-eslint';
+import rxjs from '@smarttools/eslint-plugin-rxjs';
+
+export default defineConfig([
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      tseslint.configs.recommended,
+      tseslint.configs.stylistic,
+      angular.configs.tsRecommended,
+      {
+        languageOptions: {
+          parserOptions: {
+            projectService: true,
+            tsconfigRootDir: import.meta.dirname,
+          },
+        },
+      },
+    ],
+    plugins: {
+      rxjs,
+    },
+    processor: angular.processInlineTemplates,
+    'rules': {
+      '@typescript-eslint/explicit-function-return-type': [
+        'error'
+      ],
+      'no-extra-boolean-cast': [
+        'off'
+      ],
+      '@angular-eslint/prefer-inject': [
+        'off'
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          'type': 'element',
+          'prefix': '',
+          'style': 'kebab-case'
+        }
+      ],
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          'type': 'attribute',
+          'prefix': '',
+          'style': 'camelCase'
+        }
+      ],
+      'comma-dangle': [
+        'error',
+        {
+          'objects': 'never',
+          'arrays': 'always-multiline',
+          'functions': 'never'
+        }
+      ],
+      'rxjs/finnish': [
+        'error',
+        {
+          'functions': true,
+          'methods': true,
+          'names': {
+            '^(canActivate|canActivateChild|canDeactivate|canLoad|intercept|resolve|validate)$': false
+          },
+          'parameters': true,
+          'properties': true,
+          'strict': false,
+          'types': {
+            '^EventEmitter$': false
+          },
+          'variables': true
+        }
+      ],
+      'rxjs/no-implicit-any-catch': [
+        'error',
+        {
+          'allowExplicitAny': true
+        }
+      ]
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [
+      angular.configs.templateRecommended,
+      angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+]);
